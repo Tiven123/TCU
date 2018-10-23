66f3641a8fa5fd8afb0e8c491891f23c75ff5a7c
Steven Ortiz <steven.alejandro@hotmail.com>
2018-10-23 00:31:41 -0600

    Conexion de postgresql
    Se crea la Clase de Conexion a PostgreSQL

---
+++ PuntoVenta/src/BaseDatos/Conexion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Steven Ortiz
+ */
+public class Conexion {
+
+    // Almacenamos los datos de la conexion
+    String url = "localhost:3000";
+    String usuario = "root";
+    String contrasena = "1234";
+
+    public void Iniciar() {
+        try {
+            // We register the PostgreSQL driver
+            // Registramos el driver de PostgresSQL
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error al registrar el driver de PostgreSQL: " + ex);
+            }
+            Connection connection = null;
+            // Database connect
+            // Conectamos con la base de datos 
+            connection = DriverManager.getConnection(url, usuario, contrasena);
+
+            boolean valid = connection.isValid(50000);
+            System.out.println(valid ? "TEST OK" : "TEST FAIL");
+        } catch (java.sql.SQLException sqle) {
+            System.out.println("Error: " + sqle);
+        }
+    }
+}
