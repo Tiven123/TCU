32d941ced7f55431dcec170bc68286a5f22eb059
Steven Ortiz <steven.alejandro@hotmail.com>
2018-11-20 23:54:07 -0600

    Entidades: Producto Factura Detalle Factura

---
+++ PuntoVenta/src/Entidades/DetalleFactura.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author steven
+ */
+public class DetalleFactura {
+    
+    private int id;
+    private Factura factura;
+    private Producto producto;
+    
+}
